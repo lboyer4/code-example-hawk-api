d9efc34896d60a0c6dfa6e4fddaa62d05c3f1ad0
Lauren Boyer <lboyer4@outlook.com>
2020-01-27 11:29:01 -0700

    LandingPage is rendering

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import  LandingPage  from './LandingPage/LandingPage';
 
-const App: React.FC = () => {
+
+interface State {
+	hawk: string;
+	error: string;
+	}
+
+
+class App extends React.Component {
+	state = { 
+		hawk: 'hawkone',
+		error: ''
+	};
+
+componentDidMount () {
+	console.log('hello', this.state.hawk)
+	fetch('http://localhost:8000/api/hawk/list')
+		.then(response => response.json())
+		.then(response => console.log('response', response))
+		.catch(error => (console.log('error', error)))
+}
+render() {
+
   return (
-    <div className="App">
+    <div>
       <h1>hello world</h1>
-    </div>
+      	<LandingPage />
+		</div>    
   );
 }
+}
+
 
 export default App;
---
+++ ui/src/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+
+class LandingPage extends React.Component {
+	render() {
+		return (
+			<main className="landing-page">
+			</main>
+		)
+	}
+}
+
+export default LandingPage;
