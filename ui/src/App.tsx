4cc915b29a5fdffd1286bd1959fd49ffeef16189
Lauren Boyer <lboyer4@outlook.com>
2020-01-29 12:44:54 -0700

    Added a header for the landing page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -26,7 +26,14 @@ render() {
 
   return (
     <div>
-      <h1>hello world</h1>
+    	<header>
+      	<h1>Hawk<span className="eye">Eye</span></h1>
+      	<div className="btn-holder">
+      		<button className="add-bird-btn">Add A Bird</button>
+      		<span className="divide"> | </span>
+      		<button className="view-birds-btn"> View Birds</button>
+      	</div>
+      </header>
       	<LandingPage />
 		</div>    
   );
