8bf1df589baacb795765a73dbf31b178cd0da93b
Lauren Boyer <lboyer4@outlook.com>
2020-01-30 15:06:19 -0700

    Handle Change function is handling change of inputs in the form

--- ui/src/Form/Form.tsx
+++ ui/src/Form/Form.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import './Form.css';
 
+interface State {
+	name: string;
+	size: string;
+	gender: string;
+	description: string;
+}
 class Form extends React.Component {
+	state = {
+		name: '',
+		size: '',
+		gender: '',
+		description: '',
+	}
+
+	handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+    this.setState({[name]: value}) 
+	}
+	//handleChange function
+	//handles the change of an input
+
+	//handleSubmit function
+	//posts to the backend 
+
 	render() {
 		return (
 			<form>
@@ -9,13 +32,33 @@ class Form extends React.Component {
 					<h2>Create Bird Sighting</h2>
 				</div>
 				<label>Name</label>
-				<input />
+				<input
+					type="text"
+          onChange={this.handleChange}
+          value={this.state.name}
+          placeholder='Enter Name'
+          name='name' />
 				<label>Size</label>
-				<input />
+				<input 
+					type="text"
+          onChange={this.handleChange}
+          value={this.state.size}
+          placeholder='Enter Size'
+          name='size'/>
 				<label>Gender</label>
-				<input />
+				<input 
+					type="text"
+          onChange={this.handleChange}
+          value={this.state.gender}
+          placeholder='Enter Gender'
+          name='gender'/>
 				<label>Description</label>
-				<input /> 
+				<input 
+					type="text"
+          onChange={this.handleChange}
+          value={this.state.description}
+          placeholder='Enter Description'
+          name='description'/> 
 				<button className="submit-btn">Save</button>
 			</form>
 			)
