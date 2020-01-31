df9c0fd3a8ed2164aedb9d984a21b9a62a8a08bf
Lauren Boyer <lboyer4@outlook.com>
2020-01-30 18:54:49 -0700

    Added more details for the form

--- ui/src/Form/Form.tsx
+++ ui/src/Form/Form.tsx
@@ -5,19 +5,25 @@ interface State {
 	name: string;
 	size: string;
 	gender: string;
-	description: string;
+	colorDescription: string;
+	behaviorDescription: string;
+	habitatDescription: string;
 }
 
 // interface Props {
 // 	addBird: (bird: object) => Array<object>;
 // }
 
+
+
 class Form extends React.Component {
 	state = {
 		name: '',
 		size: '',
 		gender: '',
-		description: '',
+		colorDescription: '',
+		behaviorDescription: '',
+		habitatDescription: ''
 	}
 
 	handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,26 +54,40 @@ class Form extends React.Component {
           placeholder='Enter Name'
           name='name' />
 				<label>Size</label>
-				<input 
-					type="text"
-          onChange={this.handleChange}
-          value={this.state.size}
-          placeholder='Enter Size'
-          name='size'/>
+				<select>
+					<option value="SMALL">Small</option>
+					<option value="MEDIUM">Medium</option>
+					<option value="LARGE">Large</option>
+				</select> 
 				<label>Gender</label>
 				<select>
 					<option value="FEMALE">Female</option>
 					<option value="MALE">Male</option>
 				</select> 
-				
-				<label>Description</label>
+				<label>Bird Color</label>
 				<input 
 					type="text"
           onChange={this.handleChange}
-          value={this.state.description}
+          value={this.state.colorDescription}
           placeholder='Enter Description'
           name='description'/> 
-				<button className="submit-btn">Save</button>
+         <label>Bird Behavior</label>
+				<input 
+					type="text"
+          onChange={this.handleChange}
+          value={this.state.behaviorDescription}
+          placeholder='Enter Description'
+          name='description'/> 
+        <label>Bird Habitat</label>
+				<input 
+					type="text"
+          onChange={this.handleChange}
+          value={this.state.habitatDescription}
+          placeholder='Enter Description'
+          name='description'/> 
+				<button className="submit-btn">
+					Save
+				</button>
 			</form>
 			)
 	}
