46098c00211bc793bfbf414a251ad0f576e5ba71
Lauren Boyer <lboyer4@outlook.com>
2020-02-01 18:21:58 -0700

    All data field are passed down and displaying on BirdDetails

--- ui/src/BirdDetails/BirdDetails.tsx
+++ ui/src/BirdDetails/BirdDetails.tsx
@@ -4,13 +4,30 @@ import './BirdDetails.css';
 interface BirdDetailProps {
 	id: number;
 	name: string;
+	gender: string;
+	size: string;
+	wingspanBegin: number;
+	wingspanEnd: number;
+	weightBegin: number;
+	weightEnd: number;
+	lengthBegin: number;
+	lengthEnd: number;
+	colorDescription: string;
+	behaviorDescription: string;
+	habitatDescription: string;
+	pictureUrl: string;
 }
 
 export const BirdDetails: React.FC<BirdDetailProps> = (props: BirdDetailProps) => {
 console.log('props in details', props.name)
 	return (
 		<aside>
-		<h1>{props.name}</h1>
+		<h3>{props.name}</h3>
+		<h4>Size:</h4><h5>{props.size}</h5>
+		<h4>Gender:</h4><h5>{props.gender}</h5>
+		<h4>Color:</h4><h5>{props.colorDescription}</h5>
+		<h4>Habitat:</h4><h5>{props.habitatDescription}</h5>
+		<h4>Behavior:</h4><h5>{props.behaviorDescription}</h5>
 	</aside>
 	)
 }
